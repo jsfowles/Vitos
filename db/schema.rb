1dc78a2cc81fbd59ed8ed57a53af0aee15d27301
Jacob Fowles <js.fowles@gmail.com>
2016-09-18 16:59:37 -0600

    changed schema to text for update

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160918225623) do
+
+  create_table "vitos", force: :cascade do |t|
+    t.string   "time"
+    t.text   "updates"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
