ff9216a1ab9053458f69180cdf26da412f3c7552
Jacob Fowles <js.fowles@gmail.com>
2016-09-18 17:00:45 -0600

    changed migration

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20160918225623) do
 
   create_table "vitos", force: :cascade do |t|
     t.string   "time"
-    t.text   "updates"
+    t.string   "updates"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
