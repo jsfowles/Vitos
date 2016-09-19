5b858dedb18fa3f734580ebdcc71e3663af3644b
Jacob Fowles <js.fowles@gmail.com>
2016-09-19 09:04:08 -0600

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :vitos
+  devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'vitos#index'
+  resources :vitos
 end
